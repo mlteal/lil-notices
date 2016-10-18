b85dba4ae30539d1f63eac1c1e6a1656054859e8
Andrea Self <andrea.self@fansided.com>
2016-10-18 15:31:01 -0700

    Replace divs with spans so links in messages are clickable on mobile; move count of notifications out of anchor tag

--- assets/dist/main.js
+++ assets/dist/main.js
@@ -1,5 +1,19 @@
 jQuery( document ).ready( function( $ ) {
 
+	var count = $("#wp-admin-bar-ln_all_notices ul li").length;
+	if ( count >= 0 ) {
+		$( "#wp-admin-bar-ln_menu" ).prepend( "<span class='ln-notifications'>" + count + "</span>" );
+	}
+
+	if ( count == 0 ) {
+		$( "li#wp-admin-bar-ln_menu" ).addClass( "zero-count" );
+	}
+
+	$(".ln-notice-item div").replaceWith(function() {
+		var msgclass = $( this ).attr('class');
+		return "<span class='" + msgclass + "' >" + this.innerHTML + "</span>";
+	});
+
 	// we only need to add the click/remove the hover when we're not on "mobile"
 	if ( false == $( 'body' ).hasClass( 'mobile' ) ) {
 
@@ -13,15 +27,17 @@ jQuery( document ).ready( function( $ ) {
 			e.preventDefault();
 			$( '#wp-admin-bar-ln_menu' ).toggleClass( 'hover' );
 		} );
+
+		$( '#wp-admin-bar-ln_menu>span' ).on( 'hover', function(e) {
+			return false;
+		});
+
+		// add a click event to the span
+		$( '#wp-admin-bar-ln_menu>span' ).on( 'click', function(e) {
+			e.preventDefault();
+			$( '#wp-admin-bar-ln_menu' ).toggleClass( 'hover' );
+		} );
 	}
 
-	var count = $("#wp-admin-bar-ln_all_notices ul li").length;
-	if ( count >= 0 ) {
-		$( "#wp-admin-bar-ln_menu > a" ).prepend( "<span class='ln-notifications'>" + count + "" );
-	}
-
-	if ( count == 0 ) {
-		$( "li#wp-admin-bar-ln_menu" ).addClass( "zero-count" );
-	}
 
 } );
